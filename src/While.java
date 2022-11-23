0ced4b6f3157bb8b3d6f62f87e7000bab2f92868
sylwia <sylwiadusza280@gmail.com>
2022-11-23 19:49:37 +0100

    petla_while

---
+++ src/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
