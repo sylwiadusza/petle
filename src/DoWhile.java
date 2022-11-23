b714753146691346fc9d6f77e4826259c8530882
sylwia <sylwiadusza280@gmail.com>
2022-11-23 19:44:05 +0100

    petla_dowhile_foreach

---
+++ src/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 0;
+        do{
+            System.out.println(count);
+            count++;
+        }while(count <= 10);
+        }
+}
---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cats = new String[]{"Burek", "Mruczek", "Iskierka", "Pussy"};
+        for (String cat : cats) {
+            System.out.println(cat);
+        }
+    }
+}
